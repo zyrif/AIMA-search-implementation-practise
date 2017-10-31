c6df2ffcf383813ea103c3218f2f8d0152b9c7e3
zyrif <rubaiyat9@gmail.com>
2017-11-01 02:11:25 +0600

    initial

---
+++ BFS.py
@@ -0,0 +1,114 @@
+from utils import (
+    is_in, argmin, argmax, argmax_random_tie, probability, weighted_sampler,
+    memoize, print_table, open_data, Stack, FIFOQueue, PriorityQueue, name,
+    distance
+)
+
+from collections import defaultdict
+import math
+import random
+import sys
+import bisect
+
+
+romania_map ={
+    "Arad": {"Zerind": 75, "Timisoara": 118, "Sibiu": 140},
+    "Zerind": {"Arad": 75, "Oradea": 71},
+    "Oradea": {"Zerind": 71, "Sibiu": 151},
+    "Timisoara": {"Arad": 118, "Lugoj": 111},
+    "Lugoj": {"Timisoara": 111, "Mehadia":70},
+    "Mehadia": {"Lugoj": 70, "Dobreta": 75},
+    "Dobreta": {"Mehadia":75, "Craiova":120},
+    "Craiova": {"Dobreta": 120, "RimnicuVilcea": 146, "Pitesi": 138},
+    "RimnicuVilcea": {"Craiova": 146, "Pitesi": 97, "Sibiu":80},
+    "Sibiu": {"Arad": 140, "Oradea":151, "RimnicuVilcea": 80, "Fagaras": 99},
+    "Fagaras": {"Sibiu": 99, "Bucharest":211},
+    "Pitesi": {"Bucharest": 101, "RimnicuVilcea": 97, "Craiova": 138},
+    "Bucharest": {"Pitesi": 101, "Fagaras": 211, "Giurgiu": 90, "Urziceni": 85},
+    "Giurgiu": {"Bucharest": 90},
+    "Urziceni": {"Bucharest": 85, "Hirsova": 98, "Vaslui": 142},
+    "Hirsova": {"Urziceni": 98, "Eforie": 86},
+    "Eforie": {"Hirsova": 86},
+    "Vaslui": {"Urziceni": 142, "Iasi": 92},
+    "Iasi": {"Vaslui": 92, "Neamt": 87},
+    "Neamt": {"Iasi": 87}
+}
+
+class Queue(object):
+    """Simple FIFO queue"""
+    def __init__(self, el=None):
+        self.data = []
+        if el:
+            self.data.append(el)
+
+    def empty(self):
+        return True if len(self.data)==0 else False
+
+    def insert(self, el):
+        self.data.append(el)
+
+    def insert_all(self, el):
+        for i in el:
+            self.data.append(i)
+
+    def first(self):
+        return self.data[0]
+
+    def remove_first(self):
+        return self.data.pop(0)
+
+
+class Problem(object):
+    def ___init___(self, initial, goal=None):
+        self.initial = initial
+        self.goal = goal
+
+    def actions(self, state):
+
+    def result(self, state, action):
+
+    def goal_test(self, state):
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        return c+1
+
+class Node:
+    def ___init___(self, state, parent=None, action=None, pc=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.pc = pc
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def expand(self,problem):
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        next = problem.result(self.state, action)
+        return Node(next, self, action, problem.pc(self.pc, self.state, action, next))
+
+    def solution(self):
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+
+
+
+def bfs(problem):
+    node = Node(problem.initial)
+    if problem.goal
+    
+    
+    
